64354b85b737af101fce50ef7d58778cbdb57fd2
Amit Sharma <amittsharma4567@gmail.com>
2025-06-09 18:09:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,42 +22,48 @@ document.addEventListener("keypress", function (event) {
 
 });
 
-
+var tom1 = new Audio("./tom-1.mp3");
+var tom2 = new Audio("./tom-2.mp3");
+var tom3 = new Audio('./tom-3.mp3');
+var tom4 = new Audio('./tom-4.mp3');
+var snare = new Audio('./snare.mp3');
+var crash = new Audio('./crash.mp3');
+var kick = new Audio('./kick-bass.mp3');
 function makeSound(key) {
 
     switch (key) {
         case "w":
-            var tom1 = new Audio("./tom-1.mp3");
+            tom1.currentTime=0;
             tom1.play();
             break;
 
         case "a":
-            var tom2 = new Audio("./tom-2.mp3");
+            tom2.currentTime=0;
             tom2.play();
             break;
 
         case "s":
-            var tom3 = new Audio('./tom-3.mp3');
+            tom3.currentTime=0;
             tom3.play();
             break;
 
         case "d":
-            var tom4 = new Audio('./tom-4.mp3');
+            tom4.currentTime=0;
             tom4.play();
             break;
 
         case "j":
-            var snare = new Audio('./snare.mp3');
+            snare.currentTime=0;
             snare.play();
             break;
 
         case "k":
-            var crash = new Audio('./crash.mp3');
+            crash.currentTime=0;
             crash.play();
             break;
 
         case "l":
-            var kick = new Audio('./kick-bass.mp3');
+            kick.currentTime=0;
             kick.play();
             break;
 
